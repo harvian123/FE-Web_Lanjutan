45c9fb6270336cdb4efcff0607808853d2f06b9d
shuraa75 <harviann123@gmail.com>
2021-05-11 20:59:14 +0700

    Add, Delete

---
+++ todo-frontend/src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1>Selamat Datang</h1>
+    <div>Berikut daftar kerja kita : </div>
+    <ul>
+      <li v-for="item in todos" :key="item">{{item.nama}}<button @click ="deleteTodo(item.nama)">-</button></li> 
+    </ul>
+    <input v-model="myText" type="text"/>
+    <button @click="addTodos">Add Data</button>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+  export default {
+    data:()=>{
+      return {
+        todos: [],
+        myText:''
+      }
+    },
+    mounted: function(){
+      this.getTodos()
+    },
+    methods: {
+      getTodos(){
+        axios.get('http://localhost:3000/todo')
+        .then(result => {
+          this.todos = result.data
+        })
+      },
+      addTodos(){
+        let addItem = {nama: this.myText}
+        axios.post('http://localhost:3000/todo', addItem)
+        this.todos.push(addItem)
+      },
+      deleteTodo(nama){
+        axios.delete(`http://localhost:3000/todo/${nama}`)
+          .then(() => {
+            this.getTodos()
+        })
+      }
+    }
+  }
+</script>
